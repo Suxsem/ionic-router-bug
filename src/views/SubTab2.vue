34185debe8eaa56582e907b4fc38ea0e98ff7077
Stefano Semeraro <ssemeraro@alteanet.it>
2021-04-05 20:02:15 +0200

    Initial commit

---
+++ src/views/SubTab2.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>SubTab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">SubTab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-button @click="router.push('/tabs/tab1/subtab1')">Go to subtab1</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { useRouter } from "vue-router";
+
+export default  {
+  name: 'SubTab1',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    const router = useRouter();
+    return {
+      router
+    };
+  }
+}
+</script>
